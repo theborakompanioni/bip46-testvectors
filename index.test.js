93846d922143d95900b0779b533799ec1fa6ee84
theborakompanioni <theborakompanioni+github@gmail.com>
2024-10-09 11:02:09 +0200

    test: verify first pubkey from privkey

--- index.test.js
+++ index.test.js
@@ -1,8 +1,18 @@
-import { base64 } from '@scure/base';
-import { recoverPublicKey } from './utils'
+import { base64 } from '@scure/base'
+import * as secp from '@noble/secp256k1'
+import { bytesToHex } from '@noble/hashes/utils'
+import * as wif from 'wif'
+import { recoverPublicKey, sign } from './utils'
 import TEST_VECTORS from './test_vectors'
 
 describe('BIP-0046', () => {
+    it(`should verify pubkey from private key (m/84'/0'/0'/2/0)`, () => {
+        const privateKey = wif.decode(TEST_VECTORS.first_derived_private_key).privateKey
+        const recoveredPubkey = secp.getPublicKey(privateKey)
+
+        expect(bytesToHex(recoveredPubkey)).toBe(TEST_VECTORS.first_derived_public_key)
+    })
+
     it(`should recover public key from signature (m/84'/0'/0'/2/0)`, () => {
         const message = TEST_VECTORS.first_cert_message
         const signatures = base64.decode(TEST_VECTORS.first_cert_signature)
@@ -18,6 +28,14 @@ describe('BIP-0046', () => {
 
         expect(recoveredPubkey.toHex()).toBe(TEST_VECTORS.first_derived_public_key)
     })
+
+    /*it(`should verify signature for example endpoint`, async () => {
+        const message = TEST_VECTORS.example_endpoint_message
+        const privateKey = wif.decode(TEST_VECTORS.example_bond_certificate_private_key).privateKey
+        const signature = await sign(message, privateKey)
+
+        expect(base64.encode(signature.toCompactRawBytes())).toBe(TEST_VECTORS.example_endpoint_signature)
+    })*/
 
     it(`should recover public key from signature for example endpoint`, () => {
         const message = TEST_VECTORS.example_endpoint_message
