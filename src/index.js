057a740e6f06071361a43014913a556a6f94b08e
theborakompanioni <theborakompanioni+github@gmail.com>
2024-10-14 14:36:27 +0000

    example(index): create all timelocked addresses

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import { recoverPublicKey } from './utils.js'
+import { HDKey } from '@scure/bip32'
+import * as bip39 from '@scure/bip39'
+import { indexToDerivationPath, indexToLocktime, timelockedAddressFromLocktimeAndPublicKey } from './utils.js'
 
-const sigBase64 =
-  'H2b/90XcKnIU/D1nSCPhk8OcxrHebMCr4Ok2d2yDnbKDTSThNsNKA64CT4v2kt+xA1JmGRG/dMnUUH1kKqCVSHo='
-const cert_content =
-  'fidelity-bond-cert|020000000000000000000000000000000000000000000000000000000000000001|375'
-const sigBuffer = Buffer.from(sigBase64, 'base64')
+const mnemonic = 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about'
 
-const recoveredPubkey = recoverPublicKey(cert_content, sigBuffer)
-console.log(recoveredPubkey.toHex())
+;(async function() {
+  const HD_KEY = HDKey.fromMasterSeed(
+    await bip39.mnemonicToSeed(mnemonic),
+  )
 
-if (
-  recoveredPubkey.toHex() ===
-  '02a1b09f93073c63f205086440898141c0c3c6d24f69a18db608224bcf143fa011'
-) {
-  console.log('SUCCESS: Public Key matches!')
-} else {
-  console.log('ERROR: Public Key does not match!')
-}
+  for (let index = 0; index < 960; index++) {
+    const locktime = indexToLocktime(index)
+    const path = indexToDerivationPath(index)
+    const derivedKey = HD_KEY.derive(path)
+    const address = timelockedAddressFromLocktimeAndPublicKey(locktime, derivedKey.publicKey)
+    console.log(`${index}: ${address}`)
+  }
+})()
